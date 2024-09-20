1981fb1e9bb0f9e8ca2053e90eecf81f24bb10bb
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-09-20 13:19:57 -0700

    Write pseudocode/plan logic

---
+++ rps.js
@@ -0,0 +1,29 @@
+/*
+Goal: create rock paper scissors
+- user selects a choice
+- computer selects a choice
+- winner is determined
+
+Plan: 
+The first iteration of this project will run on the command line.
+Ther user will input a choice and output if they won or lost against the 
+computer's choice.
+*/
+
+
+//Get computer's choice
+    //randomly generate a numer from 1-100
+    //depending on outcome, assign 'rock', 'paper', or 'scissors
+
+// get user input
+//Initialize a variable and set it's value to user input
+//Initialize a bool variable for a loop
+//loop until input is valid
+//Check if the input is valid (rock, paper, or scissors)
+    //If the input is valid exit loop
+    //Re-prompt the user for input if input in invalid
+
+//Compare user and computer inputs
+//the win condition is as follows: rock > scissors > paper > rock
+
+
