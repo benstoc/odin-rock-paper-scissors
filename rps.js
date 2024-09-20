bf8a98e9af0998d0ec969e5a35995d6fbde5502b
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-09-20 13:45:56 -0700

    Write logic for human choice

--- rps.js
+++ rps.js
@@ -10,11 +10,6 @@ Ther user will input a choice and output if they won or lost against the
 computer's choice.
 */
 
-
-//Get computer's choice
-    //randomly generate a number
-    //depending on outcome, assign 'rock', 'paper', or 'scissors
-
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) return 'rock';
@@ -22,27 +17,18 @@ function getComputerChoice() {
     if (choice === 2) return 'scissors';
 }
 
-// get user input
-//Initialize a variable and set it's value to user input
-//Initialize a bool variable for a loop
-//loop until input is valid
-//Check if the input is valid (rock, paper, or scissors)
-    //If the input is valid exit loop
-    //Re-prompt the user for input if input in invalid
-
 function getHumanChoice() {
-    let choice
+    let choice;
     while (true) {
-        choice = prompt('rock, paper, or scissors?')
-        choice = choice.toLowerCase()
+        choice = prompt('rock, paper, or scissors?');
+        choice = choice.toLowerCase();
         if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
-            return choice
+            return choice;
         } else {
-            alert(' Invalid input!!' )
+            alert(' Invalid input!!' );
         }
     }
 }
-getHumanChoice()
 
 //Compare user and computer inputs
 //the win condition is as follows: rock > scissors > paper > rock
