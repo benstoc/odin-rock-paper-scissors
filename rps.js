7fb1dfb7c6cb10eafbacb7d86bb3f87f820810d5
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2025-04-11 16:04:53 -0700

    Update UI
    Redoing foundations projects as a refesher after taking a break.

--- rps.js
+++ rps.js
@@ -1,68 +1,94 @@
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    if (choice === 0) return 'rock';
-    if (choice === 1) return 'paper';
-    if (choice === 2) return 'scissors';
-}
+const ROCK = '🪨';
+const PAPER = '📄';
+const SCISSORS = '✂️'
+let options = [ROCK, PAPER, SCISSORS];
+let gameIsOver = false;
 
-function playRound(humanChoice, computerChoice) {
-    const scoreBoard = `Score: ${humanScore} - ${computerScore}`
-    if (humanChoice === computerChoice) {
-        results.innerText = scoreBoard + `\nIt's a tie!`
-        return;
-    }
+const humanScore = document.querySelector(".player .score");
+const computerScore = document.querySelector(".computer .score");
+const roundNum = document.querySelector(".round span");
+const humanDisplay = document.querySelector(".human-choice");
+const computerDisplay = document.querySelector(".computer-choice");
+const resultDisplay = document.querySelector(".result");
 
-    let youWin = false;
+const container = document.querySelector("#choices");
+const btns = Array.from(container.childNodes);
 
-    if (
-        (humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'paper' && computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-        youWin = true;
-    }
-
-    if (youWin) {
-        humanScore++;
-
-        results.innerText = scoreBoard + `
-        You win! ${humanChoice} beats ${computerChoice}`
-    } else {
-        computerScore++;
-
-        results.innerText = scoreBoard + `
-        You lose! ${computerChoice} beats ${humanChoice}`
-    }
-}
-
-function completeGame() {
-    const message = `${humanScore} - ${computerScore}
-    Click a button to restart`
-
-    if (humanScore > computerScore) {
-        results.innerText = `You win! ${message}`
-    } else {
-        results.innerText = `Computer wins! ${message}`
-    }
-
-    humanScore = 0
-    computerScore = 0
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-let buttons = document.querySelectorAll('.option');
-let results = document.querySelector('.results');
-
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        let humanChoice = e.target.id;
-        let computerChoice = getComputerChoice()
-        playRound(humanChoice, computerChoice)
-
-        if (humanScore === 5 || computerScore === 5) {
-            completeGame()
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (gameIsOver) {
+            resetGame();
+            gameIsOver = false;
         }
-    })
-})
+
+        const humanChoice = btn.textContent;
+        playRound(humanChoice)
+    });
+});
+
+function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
+
+    const isWin = determineWinner(humanChoice, computerChoice);
+    updateDisplay(isWin, humanChoice, computerChoice);
+
+    if (humanScore.textContent === '5' || computerScore.textContent === '5') {
+        endGame();
+    } else {
+        incrementRound();
+    }
+}
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * options.length);
+    return options[choice];
+}
+
+function determineWinner(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return null;
+    } else if (
+        (humanChoice === ROCK && computerChoice === SCISSORS)
+        || (humanChoice === PAPER && computerChoice === ROCK)
+        || (humanChoice === SCISSORS && computerChoice === PAPER)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function updateDisplay(isWin, humanChoice, computerChoice) {
+    let winner;
+    let resultMessage;
+
+    if (isWin !== null) {
+        winner = isWin ? humanScore : computerScore;
+        resultMessage = `You ${isWin ? 'win' : 'lose'}! 
+                        ${isWin ? humanChoice : computerChoice} 
+                        beats ${isWin ? computerChoice : humanChoice}`;
+    } else {
+        resultMessage = "It's a tie!";
+    }
+
+    if (winner) winner.textContent = +winner.textContent + 1;
+    humanDisplay.textContent = humanChoice;
+    computerDisplay.textContent = computerChoice;
+    resultDisplay.textContent = resultMessage;
+}
+
+function incrementRound() {
+    roundNum.textContent = +roundNum.textContent + 1;
+}
+
+function resetGame() {
+    humanScore.textContent = 0;
+    computerScore.textContent = 0;
+    roundNum.textContent = "1";
+}
+
+function endGame() {
+    gameIsOver = true;
+    const result = +humanScore.textContent > +computerScore.textContent ? "You win" : "Computer wins";
+    resultDisplay.textContent = `${result}! Press ${ROCK}, ${PAPER}, or ${SCISSORS} to play again.`;
+}
